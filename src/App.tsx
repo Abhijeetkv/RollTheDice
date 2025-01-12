042b1237398f75dc7abb5d1e3b8316285a29d795
Abhijeet kumar <abhijeetkv2@gmail.com>
2025-01-13 01:04:07 +0530

    Enhance App component: implement dice rolling functionality and add VSCode settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,75 @@
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState } from 'react';
+import type { PropsWithChildren } from 'react';
 import {
-  
   ImageSourcePropType,
   StyleSheet,
   Text,
   View,
   Image,
+  Pressable,
 } from 'react-native';
-import DiceOne from '../assets/one.png'
-import DiceTwo from '../assets/two.png'
-import DiceThree from '../assets/three.png'
-import DiceFour from '../assets/four.png'
-import DiceFive from '../assets/five.png'
-import DiceSix from '../assets/six.png'
+import DiceOne from '../assets/one.png';
+import DiceTwo from '../assets/two.png';
+import DiceThree from '../assets/three.png';
+import DiceFour from '../assets/four.png';
+import DiceFive from '../assets/five.png';
+import DiceSix from '../assets/six.png';
 
 type DiceProps = PropsWithChildren<{
-  imageUrl: ImageSourcePropType
-}>
+  imageUrl: ImageSourcePropType;
+}>;
 
-const Dice = ({imageUrl}: DiceProps):JSX.Element => {
+const Dice = ({ imageUrl }: DiceProps): JSX.Element => {
   return (
     <View>
-      <Image style={styles.diceImage} source={imageUrl}/>
+      <Image style={styles.diceImage} source={imageUrl} />
     </View>
-  )
-}
+  );
+};
 
 function App(): React.JSX.Element {
+  const [diceImage, setDiceImage] = useState<ImageSourcePropType>(DiceOne);
+
+  const rollDiceOnTap = () => {
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+
+    switch (randomNumber) {
+      case 1:
+        setDiceImage(DiceOne);
+        break;
+      case 2:
+        setDiceImage(DiceTwo);
+        break;
+      case 3:
+        setDiceImage(DiceThree);
+        break;
+      case 4:
+        setDiceImage(DiceFour);
+        break;
+      case 5:
+        setDiceImage(DiceFive);
+        break;
+      case 6:
+        setDiceImage(DiceSix);
+        break;
+      default:
+        setDiceImage(DiceOne);
+        break;
+    }
+  };
 
   return (
-    <View>
-        <Text>text is here</Text>
+    <View style={styles.container}>
+      <Dice imageUrl={diceImage} />
+      <Pressable
+        onPress={rollDiceOnTap}
+        style={({ pressed }) => [
+          styles.pressable,
+          { backgroundColor: pressed ? '#d9d7ff' : '#E5E0FF' },
+        ]}
+      >
+        <Text style={styles.rollDiceButtonText}>Roll the Dice</Text>
+      </Pressable>
     </View>
   );
 }
@@ -41,26 +79,25 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#fff2f2'
-  },
-  diceContainer: {
-    margin: 12,
+    backgroundColor: '#fff2f2',
   },
   diceImage: {
     width: 200,
     height: 200,
   },
-  rollDiceButtonText: {
+  pressable: {
+    marginTop: 20,
     paddingVertical: 10,
     paddingHorizontal: 40,
-    borderWidth: 2,
     borderRadius: 8,
-    borderColor: '#E5E0FF',
+  },
+  rollDiceButtonText: {
     fontSize: 16,
     color: '#8EA7E9',
-    fontWeight: 700,
+    fontWeight: '700',
     textTransform: 'uppercase',
-  }
+    textAlign: 'center',
+  },
 });
 
 export default App;
